1dfc88b1ba77c0cedc185e738a86c2b1fdb9d9f7
Aditya <adityakumar2003birth@gmail.com>
2023-04-16 09:55:20 +0530

    ninth commit

--- dummy/server/utils/features.js
+++ dummy/server/utils/features.js
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken"
 export const sendCookie = (user, res, message, statusCode = 200) => {
 
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET)
+    const token = jwt.sign({ _id: user._id }, 'akdlfjladjf');
 
     res.status(statusCode)
         .cookie('token', token, {
             httpOnly: true, expires: new Date(Date.now() + 7*24*60 * 60 * 1000),
-            sameSite:process.env.NODE_ENV==="Development"?"lax":"none",
-            secure:process.env.NODE_ENV==="Development"?false:true,
+            sameSite:"none",
+            secure:true,
         })
         .json({
             success: true,
